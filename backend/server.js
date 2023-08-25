b9a57a0c34cdc0d13133a47ba63b414511a28ccd
Debraj Dhar <debrajdhar100@gmail.com>
2023-08-25 09:43:00 +0530

    created basic apis and frontend package

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
+app.get("/", (req, res) => {
+  res.send("Get req");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send("Get req");
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const single_chat = req.params.id;
+  res.send(single_chat);
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(8000, console.log("Server on 8000"));
