e02318448221e8b99c2fbf938b1c1b3c28dfdde2
Debraj Dhar <debrajdhar100@gmail.com>
2023-09-15 22:10:21 +0530

    bug fixes and component addition

---
+++ frontend/src/components/misc/SideDrawer.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const SideDrawer = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [loadingChat, setLoadingChat] = useState()
+  return <div>SideDrawer</div>;
+};
+
+export default SideDrawer;
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,15 +3,24 @@ import axios from "axios";
 import { useState } from "react";
 import { ChatState } from "../context/ChatProvider";
 import { Box } from "@chakra-ui/react";
+import SideDrawer from "../components/misc/SideDrawer";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const ChatPage = () => {
   const { user } = ChatState();
   return (
-    <div style={{ width: "100%" }}>
-      {/*user && <SideDrawer /> */}
-      <Box>
-        {/*user && <MyChats /> */}
-        {/*user && <ChatBox /> */}
+    <div style={{ width: "100%", color: "white" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="100%"
+        padding="10px"
+        height="91.5vh"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
       </Box>
     </div>
   );
